46558de8d70cab2794c083b952b2645f2558a20e
Filipe Laíns <lains@riseup.net>
2021-05-04 17:30:31 +0100

    tests/sdist: add tests for license inclusion
    Signed-off-by: Filipe Laíns <lains@riseup.net>

---
+++ tests/test_sdist.py
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: MIT
+
+import tarfile
+
+
+def test_license_file(package_license_file, sdist_license_file):
+    t = tarfile.open(sdist_license_file, 'r')
+
+    assert 'LICENSE' in t.getnames()
+
+    data = t.extractfile('LICENSE')
+
+    with open(package_license_file / 'LICENSE', 'rb') as f:
+        assert f.read() == data.read()
+
+
+def test_license_text(package_license_text, sdist_license_text):
+    t = tarfile.open(sdist_license_text, 'r')
+
+    assert t.extractfile('LICENSE').read() == 'inline license!'.encode()
