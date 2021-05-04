83a31c94f87ae7d95be86effe87052fc2bc41f62
Filipe Laíns <lains@riseup.net>
2021-05-04 03:37:31 +0100

    trampolim: add PEP 517 hooks
    Signed-off-by: Filipe Laíns <lains@riseup.net>

---
+++ trampolim/__init__.py
@@ -0,0 +1,54 @@
+# SPDX-License-Identifier: MIT
+
+from typing import Any, Dict, List, Optional
+
+import trampolim._build
+
+from trampolim._build import BuildError  # noqa: F401
+
+
+# get_requires
+
+
+def get_requires_for_build_sdist(
+    config_settings: Optional[Dict[str, Any]] = None,
+) -> List[str]:
+    return []
+
+
+def get_requires_for_build_wheel(
+    config_settings: Optional[Dict[str, Any]] = None,
+) -> List[str]:
+    return ['wheel']
+
+
+# prepare_metadata
+
+
+def prepare_metadata_for_build_wheel(
+    metadata_directory: trampolim._build.Path,
+    config_settings: Optional[Dict[str, Any]] = None,
+) -> str:
+    raise NotImplementedError
+
+
+# build
+
+
+def build_sdist(
+    sdist_directory: trampolim._build.Path,
+    config_settings: Optional[Dict[str, Any]] = None,
+) -> str:
+    project = trampolim._build.Project()
+    builder = trampolim._build.SdistBuilder(project)
+
+    builder.build(sdist_directory)
+    return builder.name
+
+
+def build_wheel(
+    wheel_directory: trampolim._build.Path,
+    config_settings: Optional[Dict[str, Any]] = None,
+    metadata_directory: Optional[trampolim._build.Path] = None,
+) -> str:
+    raise NotImplementedError
