80778b636ec2606ded8dc599d591523e2354efb4
Filipe Laíns <lains@riseup.net>
2021-09-30 20:53:10 +0100

    types: add Session
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- trampolim/types.py
+++ trampolim/types.py
@@ -7,9 +7,17 @@ import pep621
 
 import trampolim._metadata
 
+from trampolim._tasks import Session
+
 
 FrozenMetadata = collections.namedtuple('FrozenMetadata', [  # type: ignore[misc]
     field.name
     for cls in (pep621.StandardMetadata, trampolim._metadata.TrampolimMetadata)
     for field in dataclasses.fields(cls)
 ])
+
+
+__all__ = [
+    'FrozenMetadata',
+    'Session',
+]
